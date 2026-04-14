a11acbe4d6d4705475cda6a06aeb0fe15869da35
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-04-14 13:55:52 -0500

    Creating Nav Bar, Application Views, and importing them into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
+import { ApplicationViews } from '../views/ApplicationViews'
 import './App.css'
 
 export const App = () => {
 
   return (
     <>
-      
+      <ApplicationViews />
     </>
   )
 }
---
+++ src/components/nav/NavBar.jsx
@@ -0,0 +1,10 @@
+export const NavBar = () => {
+  
+  return (
+    <ul>
+      <li>Schedule</li>
+      <li>Rides</li>
+      <li>Settings dropdown menu</li>
+    </ul>
+  );
+};
