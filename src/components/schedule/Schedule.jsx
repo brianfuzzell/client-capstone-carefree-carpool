6288e7fb77e511a486d7ade99387c833d1fd8f35
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-04-15 15:27:51 -0500

    Schedule lists accordion view with currently scheduled rides

--- src/components/schedule/Schedule.jsx
+++ src/components/schedule/Schedule.jsx
@@ -1,11 +1,103 @@
-
+import { useEffect, useState } from "react";
+import {
+  getAllShifts,
+  getShiftsByDriverAndRiderShifts,
+} from "../../services/shiftService";
+import Accordion from "react-bootstrap/Accordion";
+import { getAllRiders } from "../../services/riderService";
 
 export const Schedule = () => {
+  const [allShifts, setAllShifts] = useState([]);
+  const [shiftsWithDetails, setShiftsWithDetails] = useState([]);
+  const [allRiders, setAllRiders] = useState([]);
 
-    return (
-        <>
-            <h2>Schedule</h2>
-            <div>Schedule list goes here</div>
-        </>
-    )
-}
+  useEffect(() => {
+    getAllShifts().then((shiftsArray) => {
+      setAllShifts(shiftsArray);
+    });
+  }, []);
+
+  useEffect(() => {
+    getShiftsByDriverAndRiderShifts().then((shiftsArray) => {
+      setShiftsWithDetails(shiftsArray);
+    });
+  }, []);
+
+  useEffect(() => {
+    getAllRiders().then((ridersArray) => {
+      setAllRiders(ridersArray);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2>Schedule</h2>
+      <h5>This Week at a Glance</h5>
+
+      <article>
+        {shiftsWithDetails.map((shift) => {
+          const riderIds = shift.riderShifts.map(
+            (riderShift) => riderShift.riderId,
+          );
+          const ridersOnThisShift = allRiders.filter((rider) =>
+            riderIds.includes(rider.id),
+          );
+          const date = new Date(shift.date);
+          const formattedDate = date.toLocaleDateString("en-US", {
+            month: "long",
+            day: "numeric",
+          });
+
+          return (
+            <Accordion defaultActiveKey="0" key={shift.id}>
+              <Accordion.Item eventKey={shift.id}>
+                <Accordion.Header>
+                  {formattedDate} {shift.morning ? "Morning" : "Afternoon"}
+                </Accordion.Header>
+                <Accordion.Body>
+                  <div>
+                    {shift.morning ? (
+                      <>
+                        <div>
+                          <strong>Driver: </strong>
+                          {shift.driver.fullName}
+                        </div>
+
+                        <div>
+                          <strong>Riders: </strong>
+                          {ridersOnThisShift
+                            .map((rider) => rider.fullName)
+                            .join(", ")}
+                        </div>
+                      </>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                  <div>
+                    {shift.afternoon ? (
+                      <>
+                        <div>
+                          <strong>Driver: </strong>
+                          {shift.driver.fullName}
+                        </div>
+                        <div>
+                          <strong>Riders: </strong>
+                          {ridersOnThisShift
+                            .map((rider) => rider.fullName)
+                            .join(", ")}
+                        </div>
+                      </>
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          );
+        })}
+      </article>
+    </>
+  );
+};
