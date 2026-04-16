abaa8b4e4efc421be73e766ad3af9c93e62fa0dd
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-04-15 19:42:11 -0500

    Removing comments

--- views/ApplicationViews.jsx
+++ views/ApplicationViews.jsx
@@ -3,13 +3,6 @@ import { useEffect, useState } from "react";
 export const ApplicationViews = () => {
   const [currentUser, setCurrentUser] = useState({});
 
-  // We set up state for this current user
-  // Then on initial render of this component, get the current user's data and keep it in local storage
-  // > then set it into state
-
-  // Need to pass the current user's state down to Ticket List. And then from Ticket List down to Ticket
-  // - passing state down through mult levels is called Prop Drilling
-
   useEffect(() => {
     const localCarpoolUser = localStorage.getItem("carpool_user");
     const carpoolUserObject = JSON.parse(localCarpoolUser);
@@ -17,7 +10,6 @@ export const ApplicationViews = () => {
     setCurrentUser(carpoolUserObject);
   }, []);
 
-  // If the current user is an employee, return the employee views. Otherwise, return the customer views
   return  (
     <></>
   );
